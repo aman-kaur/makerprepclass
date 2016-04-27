66743dd534fd6e7fdc12e4c712b75652111db592
Aman Bhullar <Aman Bhullar>
2016-04-26 21:04:13 -0700

    adding class 9

---
+++ class9/main.js
@@ -0,0 +1,35 @@
+function doubleALLNumbers(nums){
+	var i=0;
+	while(i<nums.length){
+		nums[i]=nums[i]*2;
+		i=i+1;
+	}
+	return nums;
+}
+function doubleALLNumbers3(nums){
+	for(var i=0;i<nums.length;i=i+1){
+		nums[i]=nums[i]*2;
+	}
+	return nums;
+}
+function sum(x){
+	var total=0;
+	for(var i=0;i<x.length;i=i+1){
+		total=total+x[i];
+	}
+return total;
+}
+function sum2(x){
+	var i=0;
+	var total=0;
+	while(i<x.length){
+		total=total+x[i];
+		i=i+1
+	}
+	return total;
+}
+var str=["aman","raj","mom"];
+ function longestWord(str){
+ 	return str.split[1];
+ }
+
